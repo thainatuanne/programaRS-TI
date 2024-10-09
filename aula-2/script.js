e145b049e18e582a18458dcffe2958e1458f32b3
thainatuanne <thaina-tuanne@hotmail.com>
2024-10-08 21:27:35 -0300

    add json

--- aula-2/script.js
+++ aula-2/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $("#button1").on("click", function (event) {
     $.getJSON("./data.json", function (data) {
-        $("#resultado1").append(data.name + " , ");
-        $("#resultado1").append(data.age + " anos ");
+        $("#resultado1").append(data.name + ", ");
+        $("#resultado1").append(data.age + " anos: ");
 
         for(let i in data.cars) {
             $("#resultado1").append(data.cars[i] + ", ")
@@ -10,6 +10,18 @@ $(document).ready(function () {
 
         $("#resultado1").append("<br>");
     });
+  });
+
+  $("#button2").on("click", function() {
+    $.getJSON("./data2.json", function(data) {
+        $("#resultado2").append(data.name + ", " + data.age + "<br>");
+
+        for(let i in data.friends) {
+            let friendName = data.friends[i].firstName + " " + data.friends[i].lastName;
+            $("#resultado2").append("Amigo " + (parseInt(i)+1) + ": " + friendName + "<br>");
+        }
+    });
+
   });
 
   $("#limpar").on("click", function (event) {
