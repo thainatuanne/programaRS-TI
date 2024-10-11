1b0356fa88c4085730c724a59902163ed9864b37
thainatuanne <thaina-tuanne@hotmail.com>
2024-10-10 21:49:39 -0300

    add aula3

--- aula-3/script.js
+++ aula-3/script.js
@@ -1,46 +1,50 @@
 $(document).ready(function() {
-    $.getJSON("https://randomuser.me/api/?results=10&nat=br", function(data) {
-        for (var i = 0; i < data.results.length; i++) {
-            var user = data.results[i];
-            var out = "<tr>";
-            out += "<td scope='row'>" + (i + 1) + "</td>";
-            out += "<td><img id='img" + i + "' src='" + data.results[i].picture.thumbnail + "' /></td>";
-            out += "<td>" + user.login.username + "</td>";
-            out += "<td>" + user.name.first + "</td>";
-            out += "<td>" + user.name.last + "</td>";
-            out += "<td>" + user.gender + "</td>"; // Adicionando gênero
-            out += "<td>" + user.email + "</td>";
-            out += "<td class='text-nowrap'>" + user.phone + "</td>";
-            out += "<td>" + $.trim(user.location.street.name) + ", " + user.location.street.number + "</td>";
-            out += "<td>" + user.location.city + "</td>";
-            out += "<td>" + user.location.state + "</td>";
-            out += "<td>" + user.location.country + "</td>";
-            out += "</tr>";
-            
-            $("table tbody").append(out);
-
-            $("#img" + i).on("error", function () {
-                loadImgAsBase64($(this).attr("src"), (dataURL) => {
-                    $(this).attr("src", dataURL);
-                });
+    $.getJSON("https://randomuser.me/api/?results=50&nat=br", function(data) {
+        if (!$.fn.DataTable.isDataTable('table')) {
+            $('table').dataTable({
+                "aaData": data.results,
+                "bProcessing": true,
+                "columns": [
+                    { data: null, searchable: false, render: function (data, type, row, meta) {
+                            return meta.row + 1;
+                        }
+                    },
+                    { data: "picture.thumbnail", searchable: false, render: function (data) {
+                            return '<img src="' + data + '" class="img-thumbnail" alt="User Image" style="width: 45px; height: 45px;">';
+                        }
+                    },
+                    { data: "login.username" },
+                    { data: "name.first" },  
+                    { data: "name.last" },      
+                    { data: "gender" },       
+                    { data: "email" },       
+                    { data: "phone" },      
+                    { data: "location.street.name", render: function(data, type, row) {
+                            return $.trim(row.location.street.name) + ", " + row.location.street.number;
+                        }
+                    },                          
+                    { data: "location.city" }, 
+                    { data: "location.state" }, 
+                    { data: "location.country" }
+                ],
+                "language": {
+                    "lengthMenu": "Exibir _MENU_ registros por página",
+                    "zeroRecords": "Nenhum registro encontrado",
+                    "info": "Mostrando _START_ a _END_ de _TOTAL_ inscritos",
+                    "infoEmpty": "Nenhum inscrito disponível",
+                    "infoFiltered": "(filtrado de _MAX_ inscritos no total)",
+                    "search": "Buscar:",
+                    "paginate": {
+                        "first": "Primeiro",
+                        "last": "Último",
+                        "next": "Próximo",
+                        "previous": "Anterior"
+                    }
+                },
+                "search": {
+                    "regex": true
+                }
             });
         }
     });
 });
-
-function loadImgAsBase64(url, callback) {
-    let canvas = document.createElement('canvas');
-    let img = document.createElement('img');
-    img.setAttribute('crossorigin', 'anonymous');
-    img.src = 'https://corsproxy.io/?' + url;
-    
-    img.onload = () => {
-        canvas.height = img.height;
-        canvas.width = img.width;
-        let context = canvas.getContext('2d');
-        context.drawImage(img, 0, 0);
-        let dataURL = canvas.toDataURL('image/png');
-        canvas = null;
-        callback(dataURL);
-    };
-}
